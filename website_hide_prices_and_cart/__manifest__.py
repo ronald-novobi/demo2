4fed315b08beead45b92a7ba73299932ac9d5537
ronald-novobi <91915791+ronald-novobi@users.noreply.github.com>
2021-12-01 14:32:24 -0500

    Add files via upload

---
+++ website_hide_prices_and_cart/__manifest__.py
@@ -0,0 +1,12 @@
+
+
+{
+    'name': "Hide Prices and Cart",
+    'version': "14.0",
+    'author': "Novobi",
+    'sequence': 1,
+    'summary': """""",
+    'depends': ['website'],
+    'data': ['data/header_cart_link.xml'],
+    'installable': True,
+}
